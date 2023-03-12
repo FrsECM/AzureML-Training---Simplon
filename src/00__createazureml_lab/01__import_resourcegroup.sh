81c2bfeb9d54f28662cf2b6533aff8623d9f54da
Francois Ponchon <francois.ponchon@michelin.com>
2023-03-12 10:46:40 +0100

    Add Data

--- src/00__createazureml_lab/01__import_resourcegroup.sh
+++
@@ -1,4 +0,0 @@
-# UNCOMMENT IF YOU WANT TO RE-INIT
-# rm terraform.tf*
-# EDIT with your ressourcegroup name
-terraform import azurerm_resource_group.default /subscriptions/049118e2-4814-401b-a34d-a67a35abc5a9/resourceGroups/F296849-Francois_Ponchon
